55dc4819c41f992a1bf8a9a4d22e8987d342d716
reborg <reborg@reborg.net>
2014-12-18 07:09:48 +0000

    Just project description

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject reclojure "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Clojure in Clojure, the hard way"
+  :url "https://github.com/reborg/reclojure"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
